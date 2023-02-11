95092a4452c8745718523f9e5e3959f77d739e69
Tyler Bedard <tylerbedard126@gmail.com>
2023-02-11 13:31:21 -0500

    models set up

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/social-network-api",
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
+
+// log mongo queries being executed
+mongoose.set("debug", true);
+
+app.use(require("./routes"));
+
+app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
